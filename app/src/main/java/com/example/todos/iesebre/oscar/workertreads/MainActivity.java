1e66b254d415e9038f8878ea7fc86b3473fdf877
OscarDuranX <oscarduran@iesebre.com>
2015-12-18 14:34:35 +0100

    1a versio

---
+++ app/src/main/java/com/example/todos/iesebre/oscar/workertreads/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.todos.iesebre.oscar.workertreads;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        Bitmap b = loadImageFromNetwork("http://example.com/image.png");
+                        mImageView.setImageBitmap(b);
+                    }
+                }).start();
+
+
+            }
+
+            private Bitmap loadImageFromNetwork(String url){
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+                    return bitmap;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+
+        });
+
+
+    }
+}
---
+++ app/src/test/java/com/example/todos/iesebre/oscar/workertreads/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.todos.iesebre.oscar.workertreads;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
